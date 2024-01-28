e3438159e4afc6279338f2db49d8c1658603e92a
Said zain <saidzain897@gmail.com>
2024-01-29 00:50:17 +0300

    Add cart functionality

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,23 @@
 // get data from local storage
 
 const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart")) || [];
-// const shoesFromLocalStorage = JSON.parse(localStorage.getItem("shoes")) ;
+const shoesFromLocalStorage = JSON.parse(localStorage.getItem("shoes"));
 
 // get the cart container
 
 const cartContainer = document.querySelector("main .container");
-
+if (cartFromLocalStorage.length === 0) {
+  cartContainer.innerHTML = `
+  <div class="empty-cart" style="width: 100%;">
+  <h1>Your cart is currently empty</h1>
+  <div>
+    <button style="display:block; margin:2rem auto; "
+    onclick="window.location.href='./shop.html'"
+    class="btn">Continue Shopping</button>
+  </div>
+  </div>
+  `;
+}
 // get the total price
 let totalPrice = 0;
 const div = document.createElement("div");
@@ -36,3 +47,115 @@ cartFromLocalStorage.forEach((item) => {
     `;
 });
 cartContainer.appendChild(div);
+
+// handle remove button
+
+const removeBtns = document.querySelectorAll(".remove");
+removeBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    cartFromLocalStorage.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cartFromLocalStorage));
+    shoesFromLocalStorage[index].quantity += 1;
+    localStorage.setItem("shoes", JSON.stringify(shoesFromLocalStorage));
+    location.reload();
+  });
+});
+
+// handle form
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.querySelector("input[type=text]");
+  const email = document.querySelector("input[type=email]");
+  const address = document.querySelector(".address");
+  const city = document.querySelector(".city");
+  const country = document.querySelector(".country");
+  const zip = document.querySelector(".zip");
+  const cardNumber = document.querySelector(".card-number");
+  const cardHolder = document.querySelector(".card-holder");
+  const expiryDate = document.querySelector(".expiry-date");
+  const cvv = document.querySelector(".cvv");
+  const nameValue = name.value;
+  const emailValue = email.value;
+  const addressValue = address.value;
+  const cityValue = city.value;
+  const countryValue = country.value;
+  const zipValue = zip.value;
+  const cardNumberValue = cardNumber.value;
+  const cardHolderValue = cardHolder.value;
+  const expiryDateValue = expiryDate.value;
+  const cvvValue = cvv.value;
+  const isEmpty =
+    nameValue === "" ||
+    emailValue === "" ||
+    addressValue === "" ||
+    cityValue === "" ||
+    countryValue === "" ||
+    zipValue === "" ||
+    cardNumberValue === "" ||
+    cardHolderValue === "" ||
+    expiryDateValue === "" ||
+    cvvValue === "";
+  if (isEmpty) {
+    displayAlert(" Error", "Please fill out all fields", "danger");
+  } else {
+    displayAlert("Success", "Message sent successfully", "success");
+    name.value = "";
+    email.value = "";
+    address.value = "";
+    city.value = "";
+    country.value = "";
+    zip.value = "";
+    cardNumber.value = "";
+    cardHolder.value = "";
+    expiryDate.value = "";
+    cvv.value = "";
+    console.log(`
+        Name: ${nameValue}
+        Email: ${emailValue}
+        Address: ${addressValue}
+        City: ${cityValue}
+        Country: ${countryValue}
+        Zip: ${zipValue}
+        Card Number: ${cardNumberValue}
+        Card Holder: ${cardHolderValue}
+        Expiry Date: ${expiryDateValue}
+        CVV: ${cvvValue}
+        `);
+    localStorage.setItem("cart", JSON.stringify([]));
+    setTimeout(() => {
+      location.reload();
+    }, 3000);
+  }
+});
+
+// display alert
+
+function displayAlert(header, text, action) {
+  const alert = document.querySelector(".alert");
+  const alertH4 = document.querySelector(" .alert h4");
+  const alertP = document.querySelector(".alert p");
+  const timeLine = document.querySelector(".alert .time-line");
+
+  alert.style.display = "block";
+  alert.style.width = "clamp(350px, 50%, 450px)";
+  alert.style.opacity = "1";
+  alert.style.top = "0%";
+
+  alert.classList.add(`alert-${action}`);
+  alertH4.innerHTML = header;
+  alertP.innerHTML = text;
+  timeLine.style.opacity = "1";
+  timeLine.classList.add(`time-line-animate`);
+
+  setTimeout(() => {
+    alert.style.opacity = "0";
+    alert.style.width = "0";
+    alert.classList.remove(`alert-${action}`);
+    timeLine.classList.remove(`time-line-animate`);
+    timeLine.style.opacity = "0";
+    alert.style.top = "-100%";
+  }, 3000);
+}
